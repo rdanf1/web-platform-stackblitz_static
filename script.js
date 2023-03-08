c35ce8fe592cf9de6f26bf59797b2ab0471a8fad
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-09 00:44:20 +0100

    "Conditionnal" Try Catch... (make sense Cf Console...)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log('Hello my children !');
 try {
-  if (document.title==="Autre Page (pour les 2)") {
+/*  if (document.title==="Autre Page (pour les 2)") {*/
     document.getElementById("nav-p2").style.border="solid 8px rgb(73, 118 ,201)";
-  } 
+  /*}*/ 
 } catch {
   console.log('MRD pas cet id-iot ICI !!! :P');
 }
