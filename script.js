53583ae14bc7d6cee1acdc2b3f840c6c2681f8c4
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-09 00:29:50 +0100

    My 1st  JS !!! (  Try Catch ;) & style assignement )

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello my children !');
+try {
+  if (document.title="Autre Page (pour les 2)") {
+    document.getElementById("nav-p2").style.border="solid 8px rgb(73, 118 ,201)";
+  } 
+} catch {
+  console.log('MRD');
+}
+
+
