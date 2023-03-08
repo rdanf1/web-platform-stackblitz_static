52bbda4e52b81328ddc8237f53b8a18eaef47e37
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-09 00:38:17 +0100

    My bad ! ( assignment = is not comparison === [or ==] ))

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('Hello my children !');
 try {
-  if (document.title="Autre Page (pour les 2)") {
+  if (document.title==="Autre Page (pour les 2)") {
     document.getElementById("nav-p2").style.border="solid 8px rgb(73, 118 ,201)";
   } 
 } catch {
