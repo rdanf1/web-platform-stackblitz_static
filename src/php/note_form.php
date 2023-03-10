0ac008e21b81ffc6352b4e586a08cf77c0019594
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 20:52:58 +0100

    1 file modified

--- src/php/note_form.php
+++ src/php/note_form.php
@@ -5,8 +5,8 @@
  <body style="font-size: 8rem;"> 
     <!--APRES LES TESTS-->
     <!--<form action="./add_note.php" method="POST">-->
-    <form action="./page3.php" method="POST" style="font-size: 8rem;">
-      <input type="text" id="table_name" name="table_name" placeholder="name" />
+    <form action="./page3.php" method="POST">
+      <input type="text" id="table_name" name="table_name" placeholder="name" style="font-size: 8rem;"/>
       <input type="submit" name="create_table" id="create_table" value="create a table"/>
     </form>
 
