93f19215d3bb5c691bdfa23926cb5e8540802ae4
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 21:01:04 +0100

    8rem form and visual

--- src/php/note_form.php
+++ src/php/note_form.php
@@ -6,8 +6,10 @@
     <!--APRES LES TESTS-->
     <!--<form action="./add_note.php" method="POST">-->
     <form action="./page3.php" method="POST">
-      <input type="text" id="table_name" name="table_name" placeholder="name" style="font-size: 8rem;"/>
-      <input type="submit" name="create_table" id="create_table" value="create a table"/>
+      <input type="text" id="table_name" name="table_name" placeholder="name" 
+            style="font-size: 8rem;"/>
+      <input type="submit" name="create_table" id="create_table" value="create a table"
+            style="font-size: 8rem;"/>
     </form>
 
  </body>
