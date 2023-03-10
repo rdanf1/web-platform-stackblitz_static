506cbb9c73105c5de9328bffd22695c1cddcbede
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 17:40:27 +0100

    1 file modified

--- php/php.js
+++ php/php.js
@@ -2,7 +2,7 @@ function like(){
   $.ajax({
       type:"POST",
       data: {like:true},
-      url: "./like.php",
+      url: "/php/like.php",
       success: function(result){
           $('#count').text(result);
       }
