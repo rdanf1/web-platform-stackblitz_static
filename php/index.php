4a575d64875f622a5fb565f6b79ce5e80cd9b7f4
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 14:13:20 +0100

    font size 24 on basic test php index page

--- php/index.php
+++ php/index.php
@@ -1,8 +1,11 @@
 <html>
  <head>
-  <title>1st PHP Page</title>
+  <title>1st PHP Page (testing Motor)</title>
  </head>
  <body>
-  <?php echo'<p>Hello World</p>';?>
+  <!-- DR - Was a little small from Android -->
+  <script type="text/javascript">font-size: 24</script>
+
+  <?php echo'<p type="javascript" script(font-size Hello World</p>';?>
  </body>
 </html>
