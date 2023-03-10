3b03598ad4bb5c30abed1561cc8b11d70bb407da
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 20:20:17 +0100

    1 file modified

--- src/php/note_form.php
+++ src/php/note_form.php
@@ -1,10 +1,11 @@
 <html>
  <head>
-  <title>1st PHP Page (testing Motor)</title>
+  <title>Saisie de la note (do, ré, ...)</title>
  </head>
- <body style="font-size: 8rem;"> <!---->
-
-    <form action="./add_note.php" method="POST">
+ <body style="font-size: 8rem;"> 
+    <!--APRES LES TESTS-->
+    <!--<form action="./add_note.php" method="POST">-->
+    <form action="./page3.php" method="POST">
       <input type="text" id="table_name" name="table_name" placeholder="name" />
       <input type="submit" name="create_table" id="create_table" value="create a table"/>
     </form>
