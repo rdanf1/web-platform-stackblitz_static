03b21dd7b3f2cb46cbfd2a49c5e2939b7f509eb1
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 17:35:02 +0100

    php in php (2 files modified, 6 added, 5 deleted)

--- php/note_form.php
+++ php/note_form.php
@@ -4,19 +4,10 @@
  </head>
  <body style="font-size: 8rem;"> <!---->
 
-    <form action="./page3.php" method="POST">
+    <form action="./add_note.php" method="POST">
       <input type="text" id="table_name" name="table_name" placeholder="name" />
       <input type="submit" name="create_table" id="create_table" value="create a table"/>
     </form>
-    
-   <?php echo"<p>Hello World : $tablename </p>";?>
-
-   <!-- DR - Was a little small from Android 
-  <script type="text/stylesheet">
-    body { 
-      font-size: 24;
-    }
-  </script>-->
 
  </body>
 </html>
--- php/php.js
+++ php/php.js
@@ -2,7 +2,7 @@ function like(){
   $.ajax({
       type:"POST",
       data: {like:true},
-      url: "like.php",
+      url: "./like.php",
       success: function(result){
           $('#count').text(result);
       }
