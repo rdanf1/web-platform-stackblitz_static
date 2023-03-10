4e73fb6252689dd5e009bd8f0e6491226e5bbe12
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 20:27:50 +0100

    magnify form (tiny as is => font 8rem)

--- src/php/note_form.php
+++ src/php/note_form.php
@@ -5,7 +5,7 @@
  <body style="font-size: 8rem;"> 
     <!--APRES LES TESTS-->
     <!--<form action="./add_note.php" method="POST">-->
-    <form action="./page3.php" method="POST">
+    <form action="./page3.php" method="POST" style="font-size: 8rem;">
       <input type="text" id="table_name" name="table_name" placeholder="name" />
       <input type="submit" name="create_table" id="create_table" value="create a table"/>
     </form>
