476ffb0c9f951e6e45bba7bc5af1f575d99eef81
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-10 14:30:20 +0100

    1 file modified

--- php/index.php
+++ php/index.php
@@ -2,16 +2,16 @@
  <head>
   <title>1st PHP Page (testing Motor)</title>
  </head>
- <body> <!-- style="font-size: 24px;">-->
+ <body style="font-size: 24px;"> <!---->
 
    <?php echo'<p>Hello World</p>';?>
 
-   <!-- DR - Was a little small from Android -->
+   <!-- DR - Was a little small from Android 
   <script type="text/stylesheet">
     body { 
       font-size: 24;
     }
-  </script>
+  </script>-->
 
  </body>
 </html>
