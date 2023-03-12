00f4656b82f427e42ccd09100b50b9b9874ae3ca
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-13 00:50:49 +0100

    1 file modified

--- src/php/class/TestClass.php
+++ src/php/class/TestClass.php
@@ -35,7 +35,7 @@ for ($i = 1; $i <= 20; $i++) {
     array_push($linesToDelete, $i);
 }
 myEcho("deleting Messages " . array_slice($linesToDelete, 0));
-$ListeMsgs->delMsg($ListeMsgs->Owner(), $linesToDelete));
+$ListeMsgs->delMsg($ListeMsgs->Owner(), $linesToDelete);
 myEcho("deleted Messages N° : 3, 6, 9, 24");
 
 $list_after_del = $ListeMsgs->listMsgs();
