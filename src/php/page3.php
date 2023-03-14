cc185a984a0e297035dceb091b7bc62b59446364
rdanf1 <101021270+rdanf1@users.noreply.github.com>
2023-03-14 01:47:44 +0100

    1 file modified

--- src/php/page3.php
+++ src/php/page3.php
@@ -80,10 +80,7 @@
   
         <div class="navbar-collapse collapse" id="navbarsExample04" style="">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <a class="nav-link" href="/src/page2.html">ZOUZOUS</a>
-            </li>
-            <!--<li class="nav-item">
+           <!--<li class="nav-item">
               <a class="nav-link disabled">HS</a>
             </li>-->
             <li class="nav-item dropdown">
@@ -106,8 +103,9 @@
       <ol class="breadcrumb">
         <!-- Item du fil d'ariane -->
        <li class="breadcrumb-item"><a href="/src/index.html">Accueil</a></li>
-       <li class="breadcrumb-item acive">Page Zous</a></li>
+       <li class="breadcrumb-item"><a href="/src/page2.html>Page Zous</a></li>
        <li class="breadcrumb-item"><a href="/src/page3-B-Cards.html">Test B-Cards</a></li>     
+       <li class="breadcrumb-item active">Interactions</li>     
    </ol>
    </nav>
     <main>
